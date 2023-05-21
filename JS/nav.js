d0e6413555065e132ab5b0ce48e280c7ab97d271
Ana-Becerra21 <91336207+Ana-Becerra21@users.noreply.github.com>
2023-05-21 03:09:21 -0300

    Agregue la API del pronostico del tiempo, use KEY FRAMES en Cards, hice un enlace entre destino y transporte. Tambien agregue logo al Nav

--- JS/nav.js
+++ JS/nav.js
@@ -1,54 +1,27 @@
-/*(function(){
-const menuLinks = document.querySelector('.menu__links');
-const hamburgerMenu = document.querySelector('.menu__hamburguer');
-const menuItems = document.querySelectorAll('.menu__item');
+const apiKey = 'f2fccefaecb21af526ec014538d02336'; // Reemplaza con tu clave de API
 
-hamburgerMenu.addEventListener('click', () => {
-  menuLinks.classList.toggle('active');
-});
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=Buenos%20Aires&appid&lang=es&appid=${apiKey}&units=metric`)
+  .then(response => response.json())
+  .then(data => {
+    const temperatureElement = document.getElementById('temperature');
+    const descriptionElement = document.getElementById('description');
+    const humidityElement = document.getElementById('humidity');
 
-menuItems.forEach((menuItem) => {
-  menuItem.addEventListener('click', () => {
-    menuLinks.classList.remove('active');
+    const temperatura = data.main.temp;
+    const descripcion = data.weather[0].description;
+    const humedad = data.main.humidity;
+
+    temperatureElement.textContent = `Temperatura: ${temperatura}°C`;
+    descriptionElement.textContent = `Descripción: ${descripcion}`;
+    humidityElement.textContent = `Humedad: ${humedad}%`;
+  })
+  .catch(error => {
+    console.log('Error al obtener los datos del pronóstico del tiempo:', error);
   });
-});
 
 
-// Seleccionar elementos del DOM
-const menuBtn = document.querySelector('.menu__img');
 
 
-// Agregar evento de click al botón del menú hamburguesa
-menuBtn.addEventListener('click', () => {
-  menuLinks.classList.toggle('active');
-});
-    
-   
-
-    
-        
-})();
-
-
-(function(){
-    const listElements = document.querySelectorAll('.menu__item--show');
-    const list = document.querySelector('.menu__links');
-    const menu = document.querySelector('.menu__hamburguer');
-
-    const addClick = ()=>{
-        listElements.forEach(element =>{
-           element.addEventListener('click', ()=>{
-            
-            let submenu =element.children[1];
-            element.classList.toggle('menu__item--active');
-
-           });
-        }); 
-    }
-    
-    addClick();
-        
-})();*/
-
-
+ 
+  
 
