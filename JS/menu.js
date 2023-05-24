e686618f6b89fbc7ec8828deaf854b72aeb622b7
Ana-Becerra21 <91336207+Ana-Becerra21@users.noreply.github.com>
2023-05-24 19:13:41 -0300

    arregle JS menu hamburguesa

--- JS/menu.js
+++ JS/menu.js
@@ -1,60 +1,55 @@
-(function(){
+(function() {
     const listElements = document.querySelectorAll('.menu__item--show');
     const list = document.querySelector('.menu__links');
     const menu = document.querySelector('.menu__hamburguer');
-
-    const addClick = ()=>{
-        listElements.forEach(element =>{
-            element.addEventListener('click', ()=>{
-
-                
-                let subMenu = element.children[1];
-                let height = 0;
-                element.classList.toggle('menu__item--active');
-
-
-                if(subMenu.clientHeight === 0){
-                    height = subMenu.scrollHeight;
-                }
-
-                subMenu.style.height = `${height}px`;
-
-            });
+  
+    const addClick = () => {
+      listElements.forEach(element => {
+        element.addEventListener('click', () => {
+          let subMenu = element.querySelector('.menu__nesting');
+          let height = 0;
+          element.classList.toggle('menu__item--active');
+  
+          if (subMenu.clientHeight === 0) {
+            height = subMenu.scrollHeight;
+          }
+  
+          subMenu.style.height = `${height}px`;
         });
-    }
-
-    const deleteStyleHeight = ()=>{
-        listElements.forEach(element=>{
-
-            if(element.children[1].getAttribute('style')){
-                element.children[1].removeAttribute('style');
-                element.classList.remove('menu__item--active');
-            }
-
-        });
-    }
-
-
-    window.addEventListener('resize', ()=>{
-        if(window.innerWidth > 800){
-            deleteStyleHeight();
-            if(list.classList.contains('menu__links--show'))
-                list.classList.remove('menu__links--show');
-
-        }else{
-            addClick();
+      });
+    };
+  
+    const deleteStyleHeight = () => {
+      listElements.forEach(element => {
+        let subMenu = element.querySelector('.menu__nesting');
+  
+        if (subMenu.getAttribute('style')) {
+          subMenu.removeAttribute('style');
+          element.classList.remove('menu__item--active');
         }
-    });
-
-    if(window.innerWidth <= 800){
+      });
+    };
+  
+    window.addEventListener('resize', () => {
+      if (window.innerWidth > 800) {
+        deleteStyleHeight();
+        if (list.classList.contains('menu__links--show')) {
+          list.classList.remove('menu__links--show');
+        }
+      } else {
         addClick();
+      }
+    });
+  
+    if (window.innerWidth <= 800) {
+      addClick();
     }
-
-    menu.addEventListener('click', ()=> list.classList.toggle('menu__links--show'));
-
-
-
-})();
+  
+    menu.addEventListener('click', () => {
+      list.classList.toggle('menu__links--show');
+    });
+  })();
+  
 
 
     
